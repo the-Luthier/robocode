d5707d7bf923edb97000a9410fee05f83d492033
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-18 12:58:28 +0300

    1234

--- robocode/robocodeapp/views.py
+++ robocode/robocodeapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
 from django.contrib import messages
 import openai
 from typing import Any, cast
