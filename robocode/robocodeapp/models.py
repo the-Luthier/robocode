9f5a03a9a63077c337ec57df2a394d0a6f20dd79
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-18 12:25:45 +0300

    1234

--- robocode/robocodeapp/models.py
+++ robocode/robocodeapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# Create your model here...
+
+class CustomUser(AbstractUser):
+    is_premium = models.BooleanField(default=False)
