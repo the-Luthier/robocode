8dca6579793c9017c32f9cb473ae3ae88f6de816
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-18 12:49:48 +0300

    1234

--- robocode/robocodeapp/models.py
+++ robocode/robocodeapp/models.py
@@ -4,5 +4,21 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your model here...
 
+
+class SubscriptionTypes(models.TextChoices):
+    FREE = 'FREE', 'Free'
+    PREMIUM = 'PREMIUM', 'Premium'
+    PRO = 'PRO', 'Pro'
+    
+
 class CustomUser(AbstractUser):
     is_premium = models.BooleanField(default=False)
+    subscription_type = models.CharField(
+        max_length=7,
+        choices=SubscriptionTypes.choices,
+        default=SubscriptionTypes.FREE,
+    )
+
+
+
+    
