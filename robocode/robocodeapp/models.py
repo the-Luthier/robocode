05d832e51a1f6bfd56490430dae8e66b47b51c24
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-18 13:54:56 +0300

    1234

--- robocode/robocodeapp/models.py
+++ robocode/robocodeapp/models.py
@@ -9,10 +9,11 @@ class SubscriptionTypes(models.TextChoices):
     FREE = 'FREE', 'Free'
     PREMIUM = 'PREMIUM', 'Premium'
     PRO = 'PRO', 'Pro'
-    
+
 
 class CustomUser(AbstractUser):
     is_premium = models.BooleanField(default=False)
+    is_pro = models.BooleanField(default=False)
     subscription_type = models.CharField(
         max_length=7,
         choices=SubscriptionTypes.choices,
